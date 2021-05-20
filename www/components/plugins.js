4087e7e236610b81ac78690f9f966fb0cf331d60
KauaDrar <kauadosreisakamine@hotmail.com>
2021-05-21 06:15:24 +0900

    Criei o aplicativo de localização no mapa, utilizei do plugin de geolocalização do cordova e do mapquest.

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,26 @@
 // This is a JavaScript file
 
+$(document).on("click","#local", function(){
+  var onSuccess = function(position) {
+    mostraMapa(position.coords.latitude, position.coords.longitude)
+    };
+
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+});
+
+function mostraMapa(lat, long){
+  L.mapquest.key = 'rmXLIm5Fxj9gzsbd0L4q42aQhviiNZMg';
+
+        var map = L.mapquest.map('map', {
+          center: [lat, long],
+          layers: L.mapquest.tileLayer('map'),
+          zoom: 18
+        });
+
+        map.addControl(L.mapquest.control());
+}
